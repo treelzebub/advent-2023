85622e10adb1ddcc273e47223e801260de8d7083
treelzebub <treelzebub@users.noreply.github.com>
2023-11-30 19:27:27 -0800

    break out last year and comment out

--- src/main/kotlin/net/treelzebub/hohoho/main.kt
+++ src/main/kotlin/net/treelzebub/hohoho/main.kt
@@ -3,6 +3,11 @@ package net.treelzebub.hohoho
 import net.treelzebub.hohoho.lastyear.*
 
 fun main(vararg args: String) {
+    // lastYear()
+
+}
+
+fun lastYear() {
     Day1.go()
     Day2.go()
     Day3.go()
